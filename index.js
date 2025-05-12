160f2dac3758ef1150b9402ef6376e2070dfd9dc
Mauricio Gastón <mauuuricio@hotmail.com>
2025-05-12 10:04:37 -0300

    clear code chat socket io

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+require("dotenv").config();
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const app = require("./src/app.js");
 const socketHandler = require("./src/config/socket.config.js");
 
 const httpServer = createServer(app);
-const io = new Server(httpServer);
 
-// Socket.io configuration
+const io = new Server(httpServer, {
+  cors: {
+    origin: [
+      process.env.CLIENT_HOST,
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
+  },
+  path: "/socket.io"
+});
+
 socketHandler(io);
 
 const PORT = process.env.PORT || 8080;
-httpServer.listen(PORT, () => {
-  console.log(`Servidor en http://localhost:${PORT}`);
+httpServer.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor en http://0.0.0.0:${PORT}`);
 });
+
